8cca7ebafd53db7ef417b215105b86cfebc6bbf8
Thanh Cong <hodienthanh123@gmail.com>
2021-01-06 00:20:08 +0700

    Day

---
+++ LienHeNguoiDung.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Empity_Chinh_2
+{
+    public partial class LienHeNguoiDung : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btnGui_Click(object sender, EventArgs e)
+        {
+            if(txtsdt.Text!="" && txtNoiDung.Text!="" && txtHoTen.Text !="" &&    txtEmail.Text!="" && txtDiaChi.Text!="")
+            {
+                if (XLDL.KiemTraSo(txtsdt.Text) == true)
+                {
+                    int a = 0;
+                }
+                else
+                {
+                    Response.Write("<script> alert ('SDT phải là số ') </script>");
+                    //Response.Write("SDT phải là dô");
+
+                }
+            }
+            else
+            {
+
+                Response.Write("<script> alert ('Phải đủ dữ liệu mới liên hệ được') </script>");
+                //Response.Write("SDT phải là dô");
+            }
+
+        }
+    }
+
+}
